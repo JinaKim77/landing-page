88e4b920ae12f6c3bcc94c0604f30f367af1370b
JinaKim <gina7441@gmail.com>
2022-04-04 16:09:40 +0100

    update app.js-worked on app functionalities

--- README.md
+++ README.md
@@ -3,12 +3,9 @@
 </h1>
 <br><br>
 
-## Project Summary
-This project aims to give you real-world scenarios of manipulating the DOM. The functionality you will be using serves two purposes: to prepare you for appending dynamically added data to the DOM, and to show you how javascript can improve the usability of an otherwise static site. 
-This project barely touches the surface of what is possible, but it does use some incredibly common events, methods, and logic.
+## repository
+This project is to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
 <br><br>
 
-## What You Will Build
-You will be building a multi-section landing page. Often times, we don’t know how much content will be added to a page through a CMS or an API. To circumvent this problem, we can dynamically add the content to the page. We will be demonstrating this with the navigation menu. Additionally, dynamically building the navigation is a great precursor to understanding the virtual DOM which you will experience when you begin working with JavaScript frameworks.
-
-To improve the user experience, the section actively being viewed should be differentiated in some way. Additionally, when a user clicks on a navigation item, the item should scroll you to the appropriate section rather than giving you the default jump.
+## Copyright and License
+A Project starter code contributed by Udacity.
--- js/app.js
+++ js/app.js
@@ -13,48 +13,153 @@
  * 
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
+/*
+ Objective: Build menu.
+ 
 */
+const listContainer = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
+let i = 0;
 
-/**
- * Define Global Variables
- * 
-*/
+function createNewElements(){
+    for(i = 1; i <= sections.length; i++){
+        // Create new elements - <li> and <a>
+        const listitems = document.createElement('li');
+        const anchor = document.createElement('a');
+        
+        listitems.innerHTML="Sectiion"+i;
+    
+        // Add href nad class attribute to <a> element
+        anchor.setAttribute("class", "menu__link");
+        anchor.setAttribute("href", "#section"+i);
+    
+        // Add id to each list element to scroll to section on link click - next part
+        listitems.setAttribute("id", "s"+i);
+    
+        anchor.appendChild(listitems);
+        listContainer.appendChild(anchor);
+    }
+}
 
+/*
+ Objective: Specify that the event is only handled once. 
+ This is achieved by passing the property once to the object. 
+ If we set once to true, the event will only be fired once.
+ 
+ */
+document.addEventListener('mousemove', function() {
+    createNewElements();
+},{once : true});
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
 
 // Scroll to section on link click
+document.addEventListener('click', function(event){
+    // This line of code will prevent the default action.
+    event.preventDefault();
 
-// Set sections as active
+    // Iterate the number of sections & menu items
+    for(let i = 0; i <= sections.length; i++){
+
+        // Declare a variable for each section with its id.
+        const sec = document.getElementById('section'+i);
+
+        // Used event.target.id to check the current clicked item's id
+        // Each list has an id.
+        if(event.target.id === "s"+i){
+            console.log("s" + i + " clicked.");
+
+            // This method scrolls an element into the visible area of the browser window.
+            sec.scrollIntoView();
+        }
+    }
+});
+
+/*
+ Objective: Set sections as active - add class 'active' to section when near top of viewport.
+
+ */
+const anchors = document.querySelectorAll('a');
+let num = 0;
+function makeActive(){
+    for(num = 0; num <= sections.length; num++){
+        const box = sections[num].getBoundingClientRect();
+
+        if(box.top <= 150 && box.bottom >= 150){
+            sections[num].classList.add("active");
+            anchors[num].classList.add("active_a");
+        }else{
+            sections[num].classList.remove("active");
+            anchors[num].classList.remove("active_a");
+        }
+    }
+}
+
+document.addEventListener('scroll', function(){
+    makeActive();
+});
+
+/*
+ Objective: Hide fixed navigation bar while not scrolling (it should still be present on page load).
+ 
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+ Objective: Make sections collapsible.
+
+*/
+const sectionBox = document.getElementsByClassName('landing__container');
+const sectionHeading = document.querySelectorAll('h2');
+const contents = document.querySelectorAll('p');
+let j;
+let firstP = 0;
+let secondP = 1;
+
+// Set class name to heading for each section and their content
+for(j = 0; j < sections.length; j++){
+    const divContainer = document.createElement('div');
+    divContainer.setAttribute("class","content");
+    sectionHeading[j].setAttribute("class", "collapsible");
+    
+    // Each section has two paragraphs 
+    contents[firstP].setAttribute("class", "content"+firstP);
+    contents[secondP].setAttribute("class", "content"+secondP);
+
+    // Add these two <p> element into the <div> element which has class name called 'contect'
+    divContainer.appendChild(contents[firstP]);
+    divContainer.appendChild(contents[secondP]);
+
+    sectionBox[j].appendChild(sectionHeading[j]);
+    sectionBox[j].appendChild(divContainer);
+
+    // Update the values to keep track of each paragraph
+    firstP+=2;
+    secondP+=2;
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var k;
+
+for (k = 0; k < coll.length; k++) {
+  
+    coll[k].addEventListener("click", function() {
+
+        this.classList.toggle("active");
+
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+  });
+}
